ef50a50ca1ff714f3f56b31cf43ca67c5579ad04
Peaxmoon <sujjalboi09@gmail.com>
2024-08-21 23:01:18 +0545

    class project

---
+++ quizphpweb/class.js
@@ -0,0 +1,66 @@
+// Write a javascript
+// program to find all prime numbers between 1 and 100 using for loop
+
+// for (let i = 2; i <= 100; i++) {
+//     let prime = true;
+//     for (j = 2; j <= i / 2; j++)
+//         if (i % j == 0) {
+//             prime = false;
+//             break;
+//         } 
+//         if (prime) {
+//             console.log(i);
+//         }
+// }
+
+
+
+// Write a javascript
+// program to reverse a string using a do while loop.
+// function reverseAString() {
+//     let stringReversed = "";
+//     let wordIndex = word.length-1;
+//     do{
+//         stringReversed+= word[wordIndex];
+//         wordIndex--;
+//     }while(wordIndex >= 0);
+//     return stringReversed;
+// }
+
+// let word = 'WasingShi';
+// console.log(reverseAString(word));
+// let word = 'hello';
+// console.log(word.length)
+
+
+
+
+
+// Write a javascript
+// program to create an array of objects representing people, where each
+// object has a name and age property. Use push() to add a new person to the
+// array, and use pop() to remove the last person from the array.
+// arr = ['dog', 'snoopDogg', 'dawg', 'cat', 'bigCat', 'smallCat'];
+// people = [
+//     { name: 'ram', age: '29' },
+//     { name: 'shyam', age: '28' }
+// ]
+// people.push({name: 'sita', age: '12'});
+// console.log(people);
+// people.pop();
+// console.log(people);
+
+
+// Write a javascript
+// program to create an array of strings and remove the first element from
+// the array. Then add a new string to the beginning of the array. Using
+// shift() and unshift().
+arr = ['dog', 'snoopDogg', 'dawg', 'cat', 'bigCat', 'smallCat'];
+arr.unshift('goat');
+console.log(arr);
+arr.shift();
+console.log(arr);
+
+
+
+
